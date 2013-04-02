0a1c383809736da7772bbd04656511a1ecbcca4c
LimeByte <limebyte@twf.co.nz>
2013-04-02 17:53:31 +1300

    Initial commit.

---
+++ src/main/java/me/limebyte/battlenight/api/BattleNightPlugin.java
@@ -0,0 +1,12 @@
+package me.limebyte.battlenight.api;
+
+public interface BattleNightPlugin {
+
+    /**
+     * Gets the BattleNightAPI running with this instance of BattleNight.
+     * 
+     * @return the API
+     */
+    public BattleNightAPI getAPI();
+
+}
