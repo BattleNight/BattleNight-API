64b67ac0bf055d0c24efab587320a032eff0e469
LimeByte <limebyte@twf.co.nz>
2013-05-06 21:40:45 +1200

    Add method to get players in the lobby.

--- src/main/java/me/limebyte/battlenight/api/battle/Lobby.java
+++ src/main/java/me/limebyte/battlenight/api/battle/Lobby.java
@@ -1,5 +1,7 @@
 package me.limebyte.battlenight.api.battle;
 
+import java.util.Set;
+
 import org.bukkit.entity.Player;
 
 public interface Lobby {
@@ -7,6 +9,8 @@ public interface Lobby {
     public void addPlayer(Player player);
     
     public void removePlayer(Player player);
+    
+    public Set<String> getPlayers();
     
     public void startBattle();
     
