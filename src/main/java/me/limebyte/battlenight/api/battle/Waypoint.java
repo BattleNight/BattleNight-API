1c9850c0d474965d1fba654c249e924ab6dbd0ef
LimeByte <limebyte@twf.co.nz>
2013-08-11 18:28:04 +1200

    Add names to Waypoints.

--- src/main/java/me/limebyte/battlenight/api/battle/Waypoint.java
+++ src/main/java/me/limebyte/battlenight/api/battle/Waypoint.java
@@ -7,8 +7,12 @@ public interface Waypoint extends ConfigurationSerializable {
 
     public Location getLocation();
 
+    public String getName();
+
     public boolean isSet();
 
     public void setLocation(Location location);
+
+    public void setName(String name);
 
 }
